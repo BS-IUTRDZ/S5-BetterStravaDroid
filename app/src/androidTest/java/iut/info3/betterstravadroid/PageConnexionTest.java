0712468cb1af272504fa0b8ed32689bb0028cde2
Rémi Jauzion <remi.jauzion@iut-rodez.fr>
2024-01-22 16:00:11 +0100

    Mise en place environnement jacoco et mokito

---
+++ app/src/androidTest/java/iut/info3/betterstravadroid/PageConnexionTest.java
@@ -0,0 +1,23 @@
+package iut.info3.betterstravadroid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PageConnexionTest {
+
+    private PageConnexion pageConnexion;
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void goToInscription() {
+    }
+}
