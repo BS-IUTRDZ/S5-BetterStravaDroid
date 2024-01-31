07e595c6b3ded78c164a23a38bf9385c346eb4d3
AntoineGouzy <antoinegp.gouzy@gmail.com>
2024-01-31 15:29:59 +0100

    Gestion de la page de parcours

--- app/src/main/java/iut/info3/betterstravadroid/MainActivity.java
+++ app/src/main/java/iut/info3/betterstravadroid/MainActivity.java
@@ -1,7 +1,6 @@
 package iut.info3.betterstravadroid;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,14 +19,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // on récupère un accès sur le ViewPager défini dans la vue (le layout)
-        ViewPager2 pager = findViewById(R.id.activity_main_viewpager);
         /*
          * on associe au ViewPager un adaptateur (c'est lui qui organise le défilement
          * entre les fragments à afficher)
          */
-        pager.setAdapter(new PageAdaptateur(this));
-        pager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+        binding.activityMainViewpager.setAdapter(new PageAdaptateur(this));
+        binding.activityMainViewpager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
                 super.onPageSelected(position);
@@ -37,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setNavbarIcon(int position) {
-        Log.i("MainACtivity", "position : " + position);
         switch(position) {
             case 0 :
                 // Gestion des boutons sur la page d'accueil
@@ -49,10 +45,23 @@ public class MainActivity extends AppCompatActivity {
 
                 binding.navbar.pathButtonInactive.setVisibility(View.VISIBLE);
                 binding.navbar.pathButtonActive.setVisibility(View.INVISIBLE);
+
+                binding.activityMainViewpager.setUserInputEnabled(true);
                 break;
-//            case 1 :
-//                return .newInstance();
             case 1 :
+                // Gestion des boutons sur la page d'accueil
+                binding.navbar.homeButtonInactive.setVisibility(View.VISIBLE);
+                binding.navbar.homeButtonActive.setVisibility(View.INVISIBLE);
+
+                binding.navbar.pauseButton.setVisibility(View.INVISIBLE);
+                binding.navbar.playButton.setVisibility(View.VISIBLE);
+
+                binding.navbar.pathButtonInactive.setVisibility(View.VISIBLE);
+                binding.navbar.pathButtonActive.setVisibility(View.INVISIBLE);
+
+                binding.activityMainViewpager.setUserInputEnabled(false);
+                break;
+            case 2 :
                 // Gestion des boutons de la page liste parcours
                 binding.navbar.homeButtonInactive.setVisibility(View.VISIBLE);
                 binding.navbar.homeButtonActive.setVisibility(View.INVISIBLE);
@@ -62,9 +71,21 @@ public class MainActivity extends AppCompatActivity {
 
                 binding.navbar.pathButtonInactive.setVisibility(View.INVISIBLE);
                 binding.navbar.pathButtonActive.setVisibility(View.VISIBLE);
+
+                binding.activityMainViewpager.setUserInputEnabled(true);
                 break;
             default :
                 break;
         }
+    }
+
+    /* Appui sur le bouton accueil de la navbar */
+    public void goToHome(View view) {
+        binding.activityMainViewpager.setCurrentItem(PageAdaptateur.PAGE_ACCUEIL);
+    }
+
+    /* Appui sur le bouton parcours de la navbar */
+    public void goToPathList(View view) {
+        binding.activityMainViewpager.setCurrentItem(PageAdaptateur.PAGE_LISTE_PARCOURS);
     }
 }
