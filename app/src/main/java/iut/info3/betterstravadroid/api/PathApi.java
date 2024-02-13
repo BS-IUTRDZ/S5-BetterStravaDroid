35e45dac20531f0de774cb74f29056ae136400e2
ANTOINE LALA <antoine.lala@iut-rodez.fr>
2024-02-13 08:31:47 +0100

    gestions des intentions arrivÃee/retour

--- app/src/main/java/iut/info3/betterstravadroid/api/PathApi.java
+++ app/src/main/java/iut/info3/betterstravadroid/api/PathApi.java
@@ -4,6 +4,5 @@ public class PathApi {
 
     public static final String PATH_API_LAST = ApiConfiguration.API_BASE_URL + "path/lastPath";
     public static final String PATH_API_ID = ApiConfiguration.API_BASE_URL + "path/";
-
     public static final String PATH_API_MODIF = ApiConfiguration.API_BASE_URL + "path/modifyDescription";
 }
