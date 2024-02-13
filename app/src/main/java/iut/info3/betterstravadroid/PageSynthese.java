cf46b7e36867e61c5913c637451f3c1346ed3dda
ANTOINE LALA <antoine.lala@iut-rodez.fr>
2024-02-13 18:02:52 +0100

    ajout intention de depart sur modification + fonction gestion retour

--- app/src/main/java/iut/info3/betterstravadroid/PageSynthese.java
+++ app/src/main/java/iut/info3/betterstravadroid/PageSynthese.java
@@ -1,5 +1,8 @@
 package iut.info3.betterstravadroid;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -36,6 +39,8 @@ public class PageSynthese extends AppCompatActivity {
     private SharedPreferences preferences;
     private RequestBuilder requestBuilder;
     private ToastMaker toastMaker;
+
+    private ActivityResultLauncher<Intent> lanceur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -158,6 +163,46 @@ public class PageSynthese extends AppCompatActivity {
                             Toast.LENGTH_LONG)
                     .show();
         }
+        setContentView(binding.getRoot());
+
+        binding.topbar.ivEditIcon.setOnClickListener(view -> {toEdit(view);});
+    }
+
+    public void toEdit(View view) {
+        Intent intent =
+                new Intent(this,
+                        PageModifParcours.class);
+
+        String description = binding.cardRun.tvDescription.getText().toString();
+        String titre = binding.cardRun.tvTitre.getText().toString();
+
+        intent.putExtra("description",description );
+        intent.putExtra("titre",titre);
+        intent.putExtra("id","bouchon"); //TODO faire passer l'id du parcours selectionner
+
+        lanceur = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                this::retourModif);
+        lanceur.launch(intent);
+
+    }
+
+    public void retourModif(ActivityResult result){
+        Intent intent = result.getData();
+        String newTitre;
+        String newDescription;
+
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            newDescription = intent.getStringExtra("description");
+            newTitre = intent.getStringExtra("titre");
+        }else {
+            newDescription = binding.cardRun.tvDescription.getText().toString();
+            newTitre = binding.cardRun.tvTitre.getText().toString();
+        }
+
+        binding.cardRun.tvDescription.setText(newDescription);
+        binding.cardRun.tvDescription.setText(newTitre);
+
     }
 
 }
