d7b19d915bcb57090b617a1d1d210965176a1016
Guillaume Médard <guillaume.medard@iut-rodez.fr>
2024-03-18 15:45:35 +0100

    Modification de l'interface pour la listes des parcours

--- app/src/main/java/iut/info3/betterstravadroid/parcours/ParcoursAdaptateur.java
+++ app/src/main/java/iut/info3/betterstravadroid/parcours/ParcoursAdaptateur.java
@@ -14,13 +14,19 @@ import iut.info3.betterstravadroid.RecyclerViewInterface;
 
 public class ParcoursAdaptateur extends RecyclerView.Adapter<ParcoursViewHolder> {
 
+
+
+    private OnBottomReachedListener onBottomReachedListener;
     private final RecyclerViewInterface recyclerViewInterface;
+
+    private boolean bottomAlreadyReached;
 
     private List<ParcoursItem> lesParcours;
 
     public ParcoursAdaptateur(List<ParcoursItem> donnees, RecyclerViewInterface recyclerViewInterface) {
         lesParcours = donnees;
         this.recyclerViewInterface = recyclerViewInterface;
+        bottomAlreadyReached = false;
     }
 
     @NonNull
@@ -35,20 +41,38 @@ public class ParcoursAdaptateur extends RecyclerView.Adapter<ParcoursViewHolder>
     public void onBindViewHolder(@NonNull ParcoursViewHolder holder, int position) {
         ParcoursItem myObject = lesParcours.get(position);
 
-        holder.cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                recyclerViewInterface.onItemClick(lesParcours.get(position));
-            }
-        });
+        holder.cardView.setOnClickListener(v -> recyclerViewInterface.onItemClick(lesParcours.get(position)));
 
         holder.bind(myObject);
+
+        if (!bottomAlreadyReached && position == lesParcours.size() - 1) {
+            if (onBottomReachedListener != null) {
+                bottomAlreadyReached = true;
+                onBottomReachedListener.onBottomReached(position);
+            }
+        }
     }
 
     @Override
     public int getItemCount() {
         return lesParcours.size();
     }
+
+    public interface OnBottomReachedListener {
+
+        void onBottomReached(int position);
+
+    }
+
+    public void setOnBottomReachedListener(OnBottomReachedListener onBottomReachedListener) {
+        this.onBottomReachedListener = onBottomReachedListener;
+    }
+
+    public void resetBottomReached() {
+        bottomAlreadyReached = false; // Réactiver l'événement onBottomReached
+    }
+
+
 
 
 }
